5eb52d3f6885288d07567da29516a145dd730fba
Taras Kozii <103899071+Taras-Kozii@users.noreply.github.com>
2025-10-22 12:23:09 +0000

    ver. 1.2

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,12 @@
 import { initNavigation } from './menu.js';
 import { initHeader } from './header.js';
 import { initClose } from './close.js';
-// import { watcherAnim, watcherToggle, countAnimate } from './animation.js';
+import { watcherToggle } from './effects/animation.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initNavigation();
   initHeader();
   initClose();
-  // watcherToggle('.hero .scroll-anim', 0.3);
+  watcherToggle('.scroll-anim', 0.1);
   // watcherAnim('[data-counter]', 0.2, countAnimate, false);
 });
