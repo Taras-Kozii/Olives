55e8bf8747530647014ed84feabbb928d10f0a9b
Taras Kozii <103899071+Taras-Kozii@users.noreply.github.com>
2025-10-23 12:45:05 +0000

    done

--- scripts/main.js
+++ scripts/main.js
@@ -3,13 +3,13 @@
 import { initNavigation } from './menu.js';
 import { initHeader } from './header.js';
 import { initClose } from './close.js';
-import { watcherAnim } from './effects/animation.js';
+import { watcherAnim, watcherToggle } from './effects/animation.js';
 import { showBefore } from './functions.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initNavigation();
   initHeader();
   initClose();
-  // watcherToggle('.scroll-anim', 0.1);
+  watcherToggle('.scroll-anim', 0.25);
   watcherAnim('.show-before', 0.25, showBefore, true);
 });
